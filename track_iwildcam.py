41ceccadb6698bc861fa43994eda24bcbbd87c92
Fagner Cunha <fagner.cunha99@gmail.com>
2021-05-22 14:39:52 -0400

    Added sequencies reading functions

--- track_iwildcam.py
+++ track_iwildcam.py
@@ -20,18 +20,55 @@ Set the environment variable PYTHONHASHSEED to a reproducible value
 before you start the python process to ensure that the model trains
 or infers with reproducibility
 """
+import json
 import random
 
 from absl import app
 from absl import flags
 import numpy as np
+import pandas as pd
 
 FLAGS = flags.FLAGS
+
+flags.DEFINE_string(
+    'test_info_json', default=None,
+    help=('Path to json file containing the test information json for'
+          ' the iWildCam2021 competition'))
+
+flags.DEFINE_string(
+    'features_json', default=None,
+    help=('Path to json file containing the features for each bounding box'))
 
 if 'random_seed' not in list(FLAGS):
   flags.DEFINE_integer(
       'random_seed', default=42,
       help=('Random seed for reproductible experiments'))
+
+flags.mark_flag_as_required('test_info_json')
+flags.mark_flag_as_required('features_json')
+
+def _load_features():
+  with open(FLAGS.features_json, 'r') as json_file:
+    json_data = json.load(json_file)
+  features = pd.DataFrame(json_data)
+
+  return features
+
+def _load_seq_info():
+  with open(FLAGS.test_info_json, 'r') as json_file:
+    json_data = json.load(json_file)
+  test_set = pd.DataFrame(json_data['images'])
+
+  return test_set
+
+def track_iwildcam(test_set, features):
+  for seq_id in test_set.seq_id.unique()[:2]:
+    print("seq: ", seq_id)
+    images_ids = list(test_set[test_set.seq_id == seq_id].id)
+    features_seq = features[features.img_id.isin(images_ids)]
+    print('num images: ', len(features_seq))
+    for img_id in features_seq.img_id.unique():
+      print('image: ', img_id)
 
 def set_random_seeds():
   random.seed(FLAGS.random_seed)
@@ -39,6 +76,10 @@ def set_random_seeds():
 
 def main(_):
   set_random_seeds()
+
+  test_set = _load_seq_info()
+  features = _load_features()
+  track_iwildcam(test_set, features)
 
 if __name__ == '__main__':
   app.run(main)
